9c71c4bd75bfa097995e33a4bdb4f7e400d979a1
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-03 21:53:50 -0430

    [IMP] primer intento de optimizacion

--- 3n+1/3n.cpp
+++ 3n+1/3n.cpp
@@ -3,22 +3,61 @@
 #include <string.h>
 #include <stack>
 #include <sstream>
-
+#include <stdio.h>
 #include <algorithm> //Para el metodo reverse
 
 using namespace std;
 
 
 int main(){
-
-   
+    int tam, i, a, b, max, aux, aux2;
+    int num[1000000];
+    string par1, par2;
     string input_line;
+
+    for(i = 0; i < 999999; i++)
+        num[i] = -1;
 
     getline(cin, input_line);
     while(cin){
-        cout << input_line;
-        getline(cin, input_line);
+        
+        tam = input_line.size();
+        for( i = 0; i < tam; i++){
+            if(input_line[i] == ' ' ){
+                par1 = input_line.substr(0, i);
+                par2 = input_line.substr(i+1, tam);
+                a = atoi( par1.c_str() );
+                b = atoi( par2.c_str() );
+                break;
+            } 
+        }
+        max = -1;
+        for(aux2 = a; aux2 <= b ; aux2++){
+            i = 1;
+            //if(num[aux2] == -1){
+                aux = aux2;
+                while( aux != 1 ){
+                    if( aux % 2 == 1  ){
+                        aux = aux*3 +1;
+                    }
+                    else{
+                        aux = aux/2;
+                    }
+                    i++;
+                }
+                    //num[aux2] = i;
+            //}
+            //else
+            //    max = num[aux2];
+
+              if(i > max){
+                max = i;
+                }
+        }
+
+        cout <<  a << " " << b << " "<< max; 
         cout << endl;
+        getline(cin, input_line);
     }
     return 0;
 }
