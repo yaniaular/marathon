c39b4e55034f6cafeda11faed5885488d569abe1
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-05 21:59:56 -0430

    [IMP] algoritmo 3n +1 está correcto y óptimo

--- 3n+1/3n.cpp
+++ 3n+1/3n.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 int main(){
     unsigned long max;
-    int tam, i, a, b, aux2;
-    long int num[1000000];
+    int i, a, b, aux2;
+    long int num[1000001];
     long int aux;
-    string par1, par2;
-    string input_line;
+
+    for(i = 0; i <= 1000000; i++)
+        num[i] = -1;
 
     while(cin >> a >> b){
         
@@ -24,15 +25,22 @@ int main(){
              
         max = 0;
         for(aux2 = a; aux2 <= b ; aux2++){
+
             aux = aux2;
-            i = 1;
-            while( aux != 1 ){
-                if( aux & 1  )
-                    aux = aux*3 +1;
-                else
-                    aux = aux>>1;
-                i = i + 1;
-            }
+                if(  num[aux2] == -1){
+                    i = 1;
+                    while( aux != 1 ){
+                        if( aux & 1  )
+                            aux = aux*3 +1;
+                        else
+                            aux = aux>>1;
+                        i = i + 1;
+                    }
+                    num[aux2] = i;
+                } 
+                else{
+                    i = num[aux2];
+                }
             if( i > max ){
                 max = i;
             }
